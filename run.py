9723850742db8a6e570892ee0d32f624ba338164
WiiuLopes <wiiu.lopes@gmail.com>
2019-10-03 20:06:04 -0300

    8 - Criação do modulo de reconhecimento de fala e multiprocessing

--- run.py
+++ run.py
@@ -11,45 +11,62 @@
 # mantida sua fonte e seu autor.
 # Obrigado.
 
-from database import conectDatabase
-from tts import textToSpeech
-from actions import *
+import _thread
 import time
+import speech_recognition as sr
 
-def main():
-    try:
-        # conecta = conectDatabase()
-        # sinalGPS()
-        # pointA = -26.915143, -49.081917
-        # pointB = -26.91534000, -49.08198400
-        # bearing = calculate_initial_compass_bearing(pointA, pointB)
-        # print(bearing)
-
-        while True:
-            # posicao = buscaUltimaPosicao()
-            gpsCompass = distanciaDirecao()
-            print("teste")
+num_thread = 0
+max_loop = 5
+thread_started = False
+run = True
 
 
-    except:
-        print("Ocorreu erro aplicacao inicia em 10 segundos")
-        time.sleep(10)
-        main()
+# Funcao responsavel por ouvir e reconhecer a fala
+def ouvir_microfone(task_name):
+    global run, thread_started
+    microfone = sr.Recognizer()
+    print("Thread : %s" % (task_name))
+    while run:
+        with sr.Microphone() as source:
+            microfone.adjust_for_ambient_noise(source)
+            print("%s Diga alguma coisa: " % (task_name))
+            audio = microfone.listen(source)
+        try:
+            # Passa o audio para o reconhecedor de padroes do speech_recognition
+            frase = microfone.recognize_google(audio, language='pt-BR')
+            # Após alguns segundos, retorna a frase falada
+            if (frase.__eq__("iniciar")):
+                print("Recebido o comando para " + frase + " a Aplicação")
+                thread_started = True
+                start_application()
+            elif (frase.__eq__("parar")):
+                print("Recebido o comando para " + frase + " a Aplicação")
+                thread_started = False
+            elif (frase.__eq__("desligar")):
+                print("Recebido o comando para " + frase + " a Aplicação")
+                run = False
+            else:
+                print("O comando " + frase + " não é valido")
+        except sr.UnknownValueError:
+            print("Não entendi favor repetir")
 
 
-def sinalGPS():
-    statusSinal = True
-    br = textToSpeech()
-
-    try:
-        print("Estabelecido sinal de GPS")
-        br.say("Estabelecido sinal de GPS")
-        br.runAndWait()
-    except:
-        print("teste")
-
-    return statusSinal
+def black_vision(task_name):
+    global thread_started
+    print("Thread : %s" % (task_name))
+    while thread_started:
+        print("Executando %s" % (task_name))
+        time.sleep(2)
+    if not thread_started:
+        print("Finalizando %s" % (task_name))
 
 
-if __name__ == '__main__':
-    main()
+def start_application():
+    _thread.start_new_thread(black_vision, ("Black Vision",))
+
+
+if __name__ == "__main__":
+    _thread.start_new_thread(ouvir_microfone, ("Microfone",))
+
+    while run:
+        pass
